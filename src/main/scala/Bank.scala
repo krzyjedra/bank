50a719c8504483b8428c406ea4e30c7c26441a80
k.jedra <kozukj@gmail.com>
2022-04-23 22:12:52 +0200

    tests done, methods updated

--- src/main/scala/Bank.scala
+++ src/main/scala/Bank.scala
@@ -5,7 +5,7 @@ class Bank {
 
   def transfer(firstacc: Account, secondacc: Account, amount: Int) = {
     if (amount <= firstacc.balance) firstacc.balance - amount & secondacc.balance + amount
-    else "Current balance is not sufficient to transfer such amount"
+    else println("Current balance is not sufficient to transfer such amount")
   }
 }
 
@@ -15,14 +15,14 @@ case class Account(balance: Int, accnr: Int) {
 
   def withdraw(amount: Int) = {
     if (balance >= amount) balance - amount
-    else "Current balance is not sufficient to withdraw such amount"
+    else println("Current balance is not sufficient to withdraw such amount")
   }
 
   def statement = println(balance)
 
   def close = {
     withdraw(balance)
-    println("Your account has been closed")
+//    println("Your account has been closed")
   }
 }
 
@@ -32,7 +32,7 @@ object Main extends App {
   val account1 = Account(1000, 22042022)
   val operationDepo = account1.deposit(500)
   val account2 = Account(1200, 23042022)
-  val operationWithd = account2.withdraw(300)
+  val operationWithd = account2.withdraw(3000)
   mbank.transfer(account1, account2, 500)
   account2.statement
   account2.close
--- src/test/scala/DepositSpec.scala
+++ src/test/scala/DepositSpec.scala
@@ -3,12 +3,32 @@ import flatspec._
 import matchers._
 
 class AccountSpec extends AnyFlatSpec with should.Matchers {
-  "Account" should "return current balance after deposit" in {
+
+behavior of "Account"
+
+  it should "return current balance after deposit" in {
     val balance = 1000
     val amount = 500
     val account1 = Account(balance, 22042022)
     val actual = account1.deposit(amount)
     val expected = balance + amount
+    actual shouldBe expected
+  }
+
+  it should "return current balance after withdraw" in {
+    val balance = 1000
+    val amount = 500
+    val account1 = Account(balance, 22042022)
+    val actual = account1.withdraw(amount)
+    val expected = balance - amount
+    actual shouldBe expected
+  }
+
+  it should "withdraw all funds and close account" in {
+    val balance = 1000
+    val account1 = Account(balance, 22042022)
+    val actual = account1.close
+    val expected = 0
     actual shouldBe expected
   }
 }
